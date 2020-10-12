9c11235b4149aae768a0d015d581245d172dc21b
Joe Dobosenski <jdobo232@iastate.edu>
2020-10-12 17:08:44 -0500

    added initial UI design and included some elemenatry key binding logic

---
+++ draw_map.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+function generateBoard(){
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var img1 = new Image();
+  img1.src = './PacmanLevel.png';
+  img1.onload = function () {
+      //draw background image
+      ctx.drawImage(img1, 0, 0, canvas.width, canvas.height);
+  };
+}
+
+function bindKeyPress(){
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 37) {
+        alert('Left was pressed');
+
+    }
+    else if (event.keyCode == 38) {
+        alert('Up was pressed');
+    }
+    else if (event.keyCode == 39) {
+        alert('Right was pressed');
+    }
+    else if (event.keyCode == 40) {
+        alert('Down was pressed');
+    }
+}, true);
+}
