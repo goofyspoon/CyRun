1a574f8e71eb4becca5d2478ba2edadf1afa79e3
marissag <marissag@iastate.edu>
2020-10-16 12:11:47 -0500

    Can now enter username and works with multiple users

--- server.js
+++ server.js
@@ -1,59 +1,53 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const redis = require('redis');
-const redisStore = require('connect-redis')(session);
-const client = redis.createClient();
 const router = express.Router();
 const app = express();
 
-app.use(session({
-    secret: 'ssshhhhh',
-    //Create a new redis store:
-    store: new redisStore({ host: 'localhost', port: 6379, client: client, ttl : 260}),
-    saveUninitialized: false,
-    resave: false
-}));
-
-app.use(bodyParser.json());
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+app.use(bodyParser.json());      
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/views'));
 
-router.get('/', (req, res) => {
-    let sess = req.session;
-    if(sess.username){
+//var sess; // global session, NOT recommended
+
+router.get('/',(req,res) => {
+    if(req.session.username) {
         return res.redirect('/lobby');
     }
     res.sendFile('index.html');
 });
 
-router.post('/username', (req, res) => {
+router.post('/username',(req,res) => {
     req.session.username = req.body.username;
-    console.log("At /username");
     res.end('done');
 });
 
-router.get('/lobby', (req, res) => {
-    if(req.session.username){
-        res.write(`<h1>Welcome to Game Lobby,  ${req.session.username} </h1><br>`);
+router.get('/lobby',(req,res) => {
+    if(req.session.username) {
+        res.write(`<h1>Welcome to the game lobby, ${req.session.username} </h1><br>`);
         res.end('<a href='+'/logout'+'>Logout</a>');
-    }else{
+    }
+    else {
         res.write('<h1>Please enter username first.</h1>');
         res.end('<a href='+'/'+'>Login</a>');
     }
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout',(req,res) => {
     req.session.destroy((err) => {
-        if(err){
+        if(err) {
             return console.log(err);
         }
         res.redirect('/');
     });
+
 });
 
 app.use('/', router);
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000,() => {
     console.log(`App Started on PORT ${process.env.PORT || 3000}`);
 });
+
+//SessionBox
